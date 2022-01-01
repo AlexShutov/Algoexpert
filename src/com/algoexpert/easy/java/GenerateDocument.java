48caba931ad591a3bf4ff4aa76e3516a7d43eaf7
AlexeyShutov <shutov.alekcey@gmail.com>
2022-01-01 17:04:40 +0300

    easy- GenerateDocument

---
+++ src/com/algoexpert/easy/java/GenerateDocument.java
@@ -0,0 +1,61 @@
+package com.algoexpert.easy.java;
+
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class GenerateDocument {
+    public static void main(String[] args) {
+        GenerateDocument tb = new GenerateDocument();
+        String characters = "Bste!hetsi ogEAxpelrt x ";
+        String document = "AlgoExpert is the Best!";
+
+        List<String> charBins = new ArrayList<>();
+        List<String> documents = new ArrayList<>();
+
+        charBins.add("");
+        documents.add("");
+
+        charBins.add("Idnsllbitauiltyplq'sjuhatcawitneotaguiingitaasure           ");
+        documents.add("I quit calling it a habit and now it's just a guilty pleasure");
+
+        charBins.add(characters);
+        documents.add(document);
+
+        GenerateDocument gd = new GenerateDocument();
+        boolean isOk = true;
+        for (int i = 0; i < charBins.size(); i++) {
+            String bin = charBins.get(i);
+            String doc = documents.get(i);
+            if (gd.generateDocument(bin, doc)) {
+                System.out.println(bin + " -> " + doc + ": passed");
+            } else {
+                isOk = false;
+                System.out.println(bin + " -> " + doc + ": failed ");
+            }
+        }
+        if (!isOk) {
+            System.out.println("\nOn of cases is failed");
+        }
+    }
+
+    public boolean generateDocument(String characters, String document) {
+        Map<Character, Integer> letterBucket = new HashMap<>();
+        for (int i = 0; i < characters.length(); i++) {
+            char c = characters.charAt(i);
+            letterBucket.put(c, letterBucket.getOrDefault(c, 1) + 1);
+        }
+        // see if we can construct given word
+        for (int i = 0; i < document.length(); i++) {
+            char c = document.charAt(i);
+            if (!letterBucket.containsKey(c) || letterBucket.get(c) == 0) {
+                return false;
+            }
+            letterBucket.put(c, letterBucket.get(c) -1);
+        }
+        // Write your code here.
+        return true;
+    }
+}
