d6efc764a2d6ffb860e26d0ff0e35c0b4d67b547
AlexeyShutov <shutov.alekcey@gmail.com>
2022-01-09 05:08:14 +0300

    medium- StaircaseTraversal with call history

--- src/com/algoexpert/medium/Staircasetraversal.java
+++ src/com/algoexpert/medium/Staircasetraversal.java
@@ -11,7 +11,8 @@ public class Staircasetraversal {
                 maxStep + " is: \n" + alg.staircaseTraversal(height, maxStep));
     }
 
-    public int staircaseTraversal(int height, int maxSteps) {
+    // Not optimal solution
+    public int staircaseTraversalSmiple(int height, int maxSteps) {
         return numberOfWays(height, maxSteps);
     }
 
@@ -24,6 +25,29 @@ public class Staircasetraversal {
         for (int step = 1; step < Math.min(maxSteps, height) + 1; step++) {
             result += numberOfWays(height - step, maxSteps);
         }
+
+        return result;
+    }
+    // Solution with dynamic programming
+    public int staircaseTraversal(int height, int maxSteps) {
+        // step start from 1, we need one more item
+        int[] history = new int[height + 1];
+        history[0] = 1;
+        history[1] = 1;
+
+        return computeNumberOfWays(height, maxSteps, history);
+    }
+
+    private int computeNumberOfWays(int currHeight, int maxSteps, int[] history) {
+        if (history[currHeight] != 0) {
+            return history[currHeight];
+        }
+
+        int result = 0;
+        for (int step = 1; step < Math.min(currHeight, maxSteps) + 1; step++) {
+            result += computeNumberOfWays(currHeight - step, maxSteps, history);
+        }
+        history[currHeight] = result;
 
         return result;
     }
