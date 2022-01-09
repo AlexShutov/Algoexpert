722144ab5129bf18a39f3633a645a4d040958e92
AlexeyShutov <shutov.alekcey@gmail.com>
2022-01-09 16:53:13 +0300

    easy- MinMaxStack

---
+++ src/com/leetcode/easy/java/MinStackQuestion.java
@@ -0,0 +1,54 @@
+package com.leetcode.easy.java;
+
+import javafx.util.Pair;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class MinStackQuestion {
+    public static void main(String[] args) {
+        int[] queries = {3, 2, 1, 2, 6};
+        MinStack minStack = new MinStack();
+        minStack.push(10);
+        minStack.push(6);
+        minStack.push(5);
+        while (!minStack.isEmpty()) {
+            System.out.print(minStack.getMin() + " ");
+            minStack.pop();
+        }
+        System.out.println();
+    }
+
+    static class MinStack {
+        Deque<Pair<Integer, Integer>> mDelegateStack;
+
+        public MinStack() {
+            mDelegateStack = new ArrayDeque<>();
+        }
+
+        public void push(int val) {
+            int minValue = val;
+            if (!isEmpty()) {
+                // compare current value with the last value in stack
+                minValue = Math.min(minValue, mDelegateStack.peekLast().getValue());
+            }
+            mDelegateStack.offerLast(new Pair<>(val, minValue));
+        }
+
+        public void pop() {
+            mDelegateStack.removeLast();
+        }
+
+        public int top() {
+            return mDelegateStack.peekLast().getKey();
+        }
+
+        public int getMin() {
+            return mDelegateStack.peekLast().getValue();
+        }
+
+        public boolean isEmpty() {
+            return mDelegateStack.isEmpty();
+        }
+    }
+}
